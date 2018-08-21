626240821022c9e43854d167560f3cbfbf5f4bda
Anuj Panchal <anujpan1@gmail.com>
2018-08-21 03:07:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function sayHiToGrandma(string) {
   
   if( string === lowercase) {
     return "I can\'t hear you!"
+  } else if( string === uppercase ) {
+    
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
