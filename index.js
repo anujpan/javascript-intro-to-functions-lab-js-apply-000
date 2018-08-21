adc98ceed21bff376c89def472048859d4a6973b
Anuj Panchal <anujpan1@gmail.com>
2018-08-21 03:06:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = "hello!"
+  var lowercase = string.toLowerCase()
   
   if( string === "hello!") {
     return "I can\'t hear you!"
